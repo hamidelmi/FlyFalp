7edf208f60bea28f8500c5234470c1cac030ba50
Hamid Elmi <Hamid.elmi@gmail.com>
2015-11-13 16:07:30 +0100

    Comments added

--- src/impl/IGameClient.java
+++ src/impl/IGameClient.java
@@ -3,10 +3,26 @@ package impl;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+/**
+ * @author hamidelmi
+ * Callback interface 
+ */
 public interface IGameClient extends Remote {
+	/**
+	 * Will be called when someone hunt the fly
+	 * @param playerName
+	 * @param newPoints
+	 * @throws RemoteException
+	 */
 	void receiveFlyHunted(String playerName, int newPoints)
 			throws RemoteException;
 
+	/**
+	 * Will be called whenever the position of fly changes
+	 * @param x
+	 * @param y
+	 * @throws RemoteException
+	 */
 	void receiveFlyPosition(int x, int y) throws RemoteException;
 
 }
